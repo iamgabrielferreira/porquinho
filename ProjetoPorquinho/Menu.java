0c473ba3f749149026f54c5ac4d7055c83c026d3
Gabriel Ferreira <gferreirad321@gmail.com>
2026-09-25 12:44:09 -0300

    feat: Atualização de funcionalidade cadastrar receita

--- ProjetoPorquinho/Menu.java
+++ ProjetoPorquinho/Menu.java
@@ -7,18 +7,24 @@ public class Menu {
         Scanner scanner = new Scanner (System.in);
         ArrayList<String> descricoes = new ArrayList<>();
         ArrayList<Double> valores = new ArrayList<>();
+        ArrayList<Double> valorReceita = new ArrayList<>();
 
-        System.out.println("-- BEM-VINDO --");
+
+        System.out.println("================================");
+        System.out.println(" BEM-VINDO AO PORQUINHO");
+        System.out.println("================================");
+        System.out.println();
 
         int opcao = 0;
-        while (opcao != 5) {
+        while (opcao != 6) {
             try {
                 System.out.println("==== MENU PORQUINHO ====");
-                System.out.println("1- Cadastrar Gasto");
-                System.out.println("2- Listar Gastos");
-                System.out.println("3- Calcular Total Gasto");
-                System.out.println("4- Excluir Gasto");
-                System.out.println("5- Sair do programa");
+                System.out.println("1- Alimentar o Porquinho");
+                System.out.println("2- Cadastrar Gasto");
+                System.out.println("3- Listar Gastos");
+                System.out.println("4- Consultar Resumo Financeiro");
+                System.out.println("5- Excluir Gasto");
+                System.out.println("6- Sair do programa");
                 System.out.print("Digite a opção de sua preferência: ");
                 opcao = scanner.nextInt();
                 System.out.println();
@@ -27,23 +33,28 @@ public class Menu {
 
                 switch (opcao) {
                     case 1:
-                        CadastroGastos.cadastrar(descricoes, valores, scanner);
+                        CadastrarReceita.cadastrarReceita(valorReceita, scanner);
                         break;
 
                     case 2:
-                        ListarGasto.listarGasto(descricoes, valores);
+                        CadastroGastos.cadastrar(descricoes, valores, scanner);
                         break;
 
                     case 3:
-                        TotalGasto.totalGasto(valores);
+                        ListarGasto.listarGasto(descricoes, valores);
                         break;
 
                     case 4:
-                        ExcluirGasto.excluirGasto(descricoes, valores, scanner);
+                        TotalGasto.totalGasto(valores, valorReceita);
                         break;
 
                     case 5:
-                        System.out.println("Fechando o porquinho...");
+                        ExcluirGasto.excluirGasto(descricoes, valores, scanner);
+                        break;
+
+                    case 6:
+                        System.out.println("Fechando o Porquinho...");
+                        System.out.println("Até a próxima!");
                         break;
 
                     default:
@@ -54,6 +65,7 @@ public class Menu {
                 scanner.nextLine(); // LIMPEZA DO BUFFER
             }
         }
+        scanner.close();
     }
 }
 
